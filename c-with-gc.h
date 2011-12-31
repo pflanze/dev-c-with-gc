51d08bb38472f1dd5364d9d2cd45f9d571e6b1bf
Christian Jaeger <chrjae@gmail.com>
2011-12-31 05:48:51 +0100

    .

--- c-with-gc.h
+++ c-with-gc.h
@@ -3,3 +3,14 @@
 
 #define END_PROC() }
 
+
+#define CALL(name,...) name(__VA_ARGS__);
+//^ to be made tail . hm only  tail calls ?
+
+// hm inline lambdas ndgehgl  hm   eh hm   wl . ? .
+
+// ach weil gar nd nested defs geht at all  muss ich, lambdas ausseneben,
+// wl manual  hoisting   hll  ,
+// udn dnn alloc von closr js t ussn  f  chmn,innen.
+
+
