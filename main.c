611ed906051d42d11abec04a1cc46b31016eb9cf
Christian Jaeger <chrjae@gmail.com>
2012-01-09 18:45:01 +0100

    (cleanup)

--- main.c
+++ main.c
@@ -4,19 +4,16 @@
 
 struct f_env {
     int a;
-} f_env;
+};
 
 struct f_arg {
     int b;
     int c;
-} f_arg;
+};
 
 void f (struct f_env* env, struct f_arg* arg) {
     printf("vars %i %i %i\n", env->a, arg->b, arg->c);
 }
-
-// a procedure that takes an env, and, hm, hm.
-//typedef  generic_proc;
 
 struct {
     void(*proc)(struct f_env* env, struct f_arg* arg);
