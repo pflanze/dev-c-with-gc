cfc98082bedd2ea533496e0598cc31f6b48af915
Christian Jaeger <chrjae@gmail.com>
2012-01-09 18:56:38 +0100

    mapfn_env geht nid  (typing mit env geht garnidwl  also,  ever dyn  ding )

--- main.c
+++ main.c
@@ -21,6 +21,30 @@ struct {
 } f_closure;
 
 
+typedef void* Object;
+
+/* lists */
+
+struct pair {
+    Object car;
+    Object cdr;
+};
+// flat kinds of pairs?...  then multitude of map implementations
+// needed yeah, or dyn dispatch
+
+/* General types (*Interface*!), if C had struct equiv typing, right?,
+   or, just *do* use these in definitions? -- AH, env is (ehr, has to be) actually unchecked. */
+
+struct mapfn_env {
+
+//struct map_env not needed
+struct map_arg {
+    void(*mapfn)(struct mapfn_env* env, struct mapfn_arg* arg);
+    struct pair* lis;
+}
+
+
+
 int main () {
     struct f_env env= {
 	10
