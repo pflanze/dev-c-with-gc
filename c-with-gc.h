afe356c695f23810e89dfa589eb10eac971e0f4a
Christian Jaeger <chrjae@gmail.com>
2012-01-11 02:15:41 +0100

    (same)

--- c-with-gc.h
+++ c-with-gc.h
@@ -3,17 +3,17 @@
 #define __PICK2nd(a,...) __VA_ARGS__
 
 #define DEFINE(Rtype, Name, Env, ...)					\
-    #define NOENV {}							\
-	struct Name##_env Env;						\
-    #undef NOENV							\
+#define NOENV {}							\
+    struct Name##_env Env;						\
+#undef NOENV							\
     struct Name##_closure {						\
 	Rtype (*proc) (struct Name##_env* env,				\
 		       __VA_ARGS__);					\
 	struct Name##_env env;						\
     };									\
-    #define NOENV _,struct Name##_closure Name = { Name##_proc,	{} };	\
+#define NOENV _,struct Name##_closure Name = { Name##_proc,	{} };	\
     __PICK2nd(Env)							\
-    #undef NOENV							\
+#undef NOENV							\
     Rtype Name##_proc (struct Name##_env* env, __VA_ARGS__) 
 
 
