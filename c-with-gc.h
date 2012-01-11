bff0f5459ea06b39673d88343ceb5cbd7b09ab13
Christian Jaeger <chrjae@gmail.com>
2012-01-11 02:11:05 +0100

    ('#define NOENV' doesn't work, oh.)
    c-with-gc.h:5:37: error: '#' is not followed by a macro parameter

--- c-with-gc.h
+++ c-with-gc.h
@@ -1,16 +1,19 @@
 #define ENV(...) { __VA_ARGS__ }
 
-#define DEFINE(Rtype, Name, Env, ...)				\
-    struct Name##_env Env;					\
-    struct Name##_closure {					\
-	Rtype (*proc) (struct Name##_env* env,			\
-		       __VA_ARGS__);				\
-	struct Name##_env env;					\
-    };								\
-    struct Name##_closure Name = {				\
-	Name##_proc,						\
-	{}							\
-    };								\
+#define __PICK2nd(a,...) __VA_ARGS__
+
+#define DEFINE(Rtype, Name, Env, ...)					\
+    #define NOENV {}							\
+	struct Name##_env Env;						\
+    #undef NOENV							\
+    struct Name##_closure {						\
+	Rtype (*proc) (struct Name##_env* env,				\
+		       __VA_ARGS__);					\
+	struct Name##_env env;						\
+    };									\
+    #define NOENV _,struct Name##_closure Name = { Name##_proc,	{} };	\
+    __PICK2nd(Env)							\
+    #undef NOENV							\
     Rtype Name##_proc (struct Name##_env* env, __VA_ARGS__) 
 
 
